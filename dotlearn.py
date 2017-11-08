c9d3cc6b0c05eb3d96f100d04830f5c8cb02c79f
Sverrir <sverrir@gmx.com>
2017-11-08 14:49:55 -0500

    Step 1.1 completed!

--- dotlearn.py
+++ dotlearn.py
@@ -2,20 +2,39 @@ import nengo
 import nengo_spa as spa
 import nengo_gui
 
+import matplotlib.pyplot as plt
+
 import numpy as np
 
 dimensions = 16
+t_stop = 2
+seedy = 3
+total_time = 3
 
-vocab = spa.Vocabulary(dimensions)
+cycle_length = 0.6
+
+vocab = spa.Vocabulary(dimensions, np.random.RandomState(seedy))
+
 vocab.populate("A; B; C")
 
-
 def objective_func(t, x):
-    return x[0] - np.dot(x[1:], vocab["B"].v)
+    if t > t_stop:
+        return 0
+    else:
+        return x[0] - np.dot(x[1:], vocab["B"].v)
 
+def cycles(t):
+    c = t % cycle_length
+    if c < cycle_length / 3.0:
+        return "A"
+    elif c < 2 * cycle_length / 3.0 :
+        return "B"
+    else:
+        return "C"
 
-with spa.Network() as model:
-    model.input = spa.Transcode(output_vocab=vocab)
+with spa.Network(seed=seedy) as model:
+    model.input = spa.Transcode(cycles, output_vocab=vocab)
+
     model.state = spa.State(vocab, subdimensions=dimensions, represent_identity=False)
     assert len(model.state.all_networks) == 1
     assert len(model.state.all_networks[0].ea_ensembles) == 1
@@ -38,3 +57,26 @@ with spa.Network() as model:
                             learning_rule_type=nengo.PES())
 
     nengo.Connection(model.error, conn.learning_rule)
+
+    error_probe = nengo.Probe(model.error)
+    output_probe = nengo.Probe(model.output)
+    input_probe = nengo.Probe(model.input.output)
+
+with nengo.Simulator(model) as sim:  # Create a simulator
+    sim.run(total_time)  # Run it for 1 second
+
+plt.figure()
+plt.plot(sim.trange(), sim.data[error_probe])
+plt.title("Error")
+plt.xlim(0, total_time)
+
+plt.figure()
+plt.plot(sim.trange(), spa.similarity(sim.data[input_probe], vocab))
+plt.legend(vocab.keys(), fontsize='x-small')
+plt.ylabel('State')
+
+plt.figure()
+plt.plot(sim.trange(), sim.data[output_probe])
+plt.title("Output")
+plt.xlim(0, total_time)
+plt.show()
